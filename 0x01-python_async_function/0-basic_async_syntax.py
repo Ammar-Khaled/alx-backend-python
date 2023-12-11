036fc0102e4d30008d8913d7117aa05ca50d60c9
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-12-11 14:31:16 +0000

    Fix style

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,7 @@
 """Define wait_random function."""
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay up to max_delay seconds then returns it."""
     import random
     import asyncio
