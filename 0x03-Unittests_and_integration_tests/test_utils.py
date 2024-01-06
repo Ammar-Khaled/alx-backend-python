b3ddc7c0439633b6172942e045895fbbb1eb8902
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-06 06:21:48 +0000

    Complete tasks#0, 1: test access_nested_map util

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Test utils.access_nested_map."""
+import unittest
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test utils.access_nested_map."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
+                               expected: Any):
+        """Test access_nested_map method."""
+        access_nested_map = __import__('utils').access_nested_map
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence):
+        """Test access_nested_map exceptions."""
+        access_nested_map = __import__('utils').access_nested_map
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
