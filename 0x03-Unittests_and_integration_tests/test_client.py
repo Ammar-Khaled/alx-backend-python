d24601c29b0667854d3b5a2aa3fa09796b38d7f7
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-06 19:18:24 +0000

    Test client.has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,3 +44,13 @@ class TestGithubOrgClient(TestCase):
             self.assertEqual(client.public_repos(), ['truth', 'autoparse'])
             mock_url.assert_called_once()
             mocked_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, result):
+        """Test has_license method."""
+        GithubOrgClient = __import__('client').GithubOrgClient
+        client = GithubOrgClient('google')
+        self.assertEqual(client.has_license(repo, license_key), result)
