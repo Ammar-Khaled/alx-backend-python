754a4bfd80f610ee5a9596e44e21d5b5238924ce
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-06 18:50:04 +0000

    Test client._public_repos_url.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test client.py functions."""
 from parameterized import parameterized
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, PropertyMock
 
 
 class TestGithubOrgClient(TestCase):
@@ -19,3 +19,13 @@ class TestGithubOrgClient(TestCase):
         client.org()
         targeted_endpoint = f"https://api.github.com/orgs/{test_org}"
         mocked_get_json.assert_called_once_with(targeted_endpoint)
+
+    def test_public_repos_url(self):
+        """Test _public_repos_url."""
+        GithubOrgClient = __import__('client').GithubOrgClient
+        client = GithubOrgClient('google')
+        repos_url = "https://api.github.com/orgs/google/repos"
+        org_path = 'client.GithubOrgClient.org'
+        with patch(org_path, new_callable=PropertyMock) as mocked_org:
+            mocked_org.return_value = {"repos_url": repos_url}
+            self.assertEqual(client._public_repos_url, repos_url)
